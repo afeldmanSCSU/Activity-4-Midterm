bd8ae0fa8860a717d0b017cd4814476b5038928a
Alex Feldman <79878358+afeldmanSCSU@users.noreply.github.com>
2024-03-10 15:39:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-asdasd
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add_nums(num1, num2):
+  sum = num1 + num2
+  return str(sum)
+
+@app.route('/subtract/<int:num1>/<int:num2>')
+def subtract_nums(num1, num2):
+  sum = num1 - num2
+  return str(sum)
+
+@app.route('/multiply/<int:num1>/<int:num2>')
+def multiply_nums(num1, num2):
+  sum = num1 * num2
+  return str(sum)
+
+@app.route('/divide/<int:num1>/<int:num2>')
+def divide_nums(num1, num2):
+  sum = num1 / num2
+  return str(sum)
